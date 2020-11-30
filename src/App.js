f9938e98144176bbdad5dd7a60338413c7289c5c
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2020-11-30 15:29:08 -0500

    Adding tutorial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center py-4 bg-light">
+      <h1>My Simple Login Form</h1>
+      <form className="my-5">
+        <input type="text" placeholder="Enter email address" className="form-control my-2" />
+        <input type="password" placeholder="Enter password" className="form-control my-2" />
+        <input type="submit" value="Login" className="btn btn-warning btn-lg form-control" />
+      </form>
     </div>
   );
 }
